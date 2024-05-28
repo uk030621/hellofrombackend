38ddaf2fe04c47faf8ad95b5ca6668703b1cc99a
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-05-28 22:45:49 +0100

    Initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// pages/api/hello.js
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ message: 'Hello from the backend!' });
 }
